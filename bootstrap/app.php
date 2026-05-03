8923adcb55dc4644b0d4761787f7c714b5912bf2
samiur2022 <samiuriubatcse@gmail.com>
2026-05-03 20:22:16 +0600

    update_bosstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -30,7 +30,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         // Global web middleware append
         $middleware->web(append: [
-            \App\Http\Middleware\CheckCountry::class,
+            // \App\Http\Middleware\CheckCountry::class,  // ← Temporarily disabled
             \App\Http\Middleware\TrackUserActivity::class,
         ]);
     })
